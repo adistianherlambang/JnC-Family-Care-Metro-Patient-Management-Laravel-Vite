cb734f954dc0ff26ec1afd67f6a0d13c7e798426
Adistian Herlambang <adistian59@gmail.com>
2026-08-20 11:55:04 +0700

    ubah tampilan navigasi dashboard admin menjadi mobile app dengan bottom nav di bagian bawah saat tampilan mobile

--- resources/js/components/DashboardLayout/DashboardLayout.jsx
+++ resources/js/components/DashboardLayout/DashboardLayout.jsx
@@ -63,33 +63,38 @@ export default function DashboardLayout({
   return (
     <div className={styles.container}>
       <div className={styles.thirdContainer}>
-        {/* Sidebar Menu */}
+        {/* Sidebar Menu (Desktop) */}
         <aside className={styles.menuWrapper}>
           <div className={styles.logo}>
             <img src="/logo.png" alt="Logo" />
           </div>
-          {menuItems.map((item) => (
-            <div
-              key={item.id}
-              className={`${styles.menuItem} ${activeMenu === item.id ? styles.menuItemActive : ""}`}
-              onClick={() => onMenuChange && onMenuChange(item.id)}
-            >
-              {item.svg}
-              {item.label}
-            </div>
-          ))}
-          <div className={styles.menuItem} onClick={handleLogout}>
+          <div className={styles.menuListScroll}>
+            {menuItems.map((item) => (
+              <div
+                key={item.id}
+                className={`${styles.menuItem} ${activeMenu === item.id ? styles.menuItemActive : ""}`}
+                onClick={() => onMenuChange && onMenuChange(item.id)}
+              >
+                {item.svg}
+                <span>{item.label}</span>
+              </div>
+            ))}
+          </div>
+          <div className={styles.menuItemLogout} onClick={handleLogout}>
             <svg width="12" height="16" viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M11.1429 0H0.857143C0.629814 0 0.411797 0.0842854 0.251051 0.234315C0.090306 0.384344 0 0.587827 0 0.8V8L4.28571 4.8V7.2H9.42857V8.8H4.28571V11.2L0 8V15.2C0 15.4122 0.090306 15.6157 0.251051 15.7657C0.411797 15.9157 0.629814 16 0.857143 16H11.1429C11.3702 16 11.5882 15.9157 11.7489 15.7657C11.9097 15.6157 12 15.4122 12 15.2V0.8C12 0.587827 11.9097 0.384344 11.7489 0.234315C11.5882 0.0842854 11.3702 0 11.1429 0Z" fill="currentColor" />
             </svg>
-            Keluar
+            <span>Keluar</span>
           </div>
         </aside>
 
         {/* Main Content Wrapper */}
         <div className={styles.rightWrapper}>
-          {/* Top Navbar */}
+          {/* Top Navbar Header */}
           <header className={styles.nav}>
+            <div className={styles.mobileHeaderLogo}>
+              <img src="/logo.png" alt="Logo" />
+            </div>
             <div className={styles.userInfoWrapper}>
               <div className={styles.avatarContainer}>
                 {renderAvatar()}
@@ -101,6 +106,11 @@ export default function DashboardLayout({
                 </p>
                 {userInfo.subtitle && <p className={styles.userSubtitle}>{userInfo.subtitle}</p>}
               </div>
+              <div className={styles.mobileLogoutBtn} onClick={handleLogout} title="Keluar">
+                <svg width="14" height="16" viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M11.1429 0H0.857143C0.629814 0 0.411797 0.0842854 0.251051 0.234315C0.090306 0.384344 0 0.587827 0 0.8V8L4.28571 4.8V7.2H9.42857V8.8H4.28571V11.2L0 8V15.2C0 15.4122 0.090306 15.6157 0.251051 15.7657C0.411797 15.9157 0.629814 16 0.857143 16H11.1429C11.3702 16 11.5882 15.9157 11.7489 15.7657C11.9097 15.6157 12 15.4122 12 15.2V0.8C12 0.587827 11.9097 0.384344 11.7489 0.234315C11.5882 0.0842854 11.3702 0 11.1429 0Z" fill="currentColor" />
+                </svg>
+              </div>
             </div>
           </header>
 
@@ -110,6 +120,26 @@ export default function DashboardLayout({
           </main>
         </div>
       </div>
+
+      {/* Mobile Bottom App Navigation Bar */}
+      <nav className={styles.mobileBottomNav}>
+        <div className={styles.mobileBottomNavInner}>
+          {menuItems.map((item) => {
+            const isActive = activeMenu === item.id;
+            return (
+              <button
+                key={item.id}
+                type="button"
+                className={`${styles.mobileNavItem} ${isActive ? styles.mobileNavItemActive : ""}`}
+                onClick={() => onMenuChange && onMenuChange(item.id)}
+              >
+                <div className={styles.mobileNavIcon}>{item.svg}</div>
+                <span className={styles.mobileNavLabel}>{item.label}</span>
+              </button>
+            );
+          })}
+        </div>
+      </nav>
     </div>
   );
 }
